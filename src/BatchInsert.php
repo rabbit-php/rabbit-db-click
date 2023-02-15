7a8ce0d900c2a2d4f51fb6ec9cfa2c21fb58c49a
Albert <amuluowin@163.com>
2023-02-15 10:45:13 +0800

    fix BatchInsert

--- src/BatchInsert.php
+++ src/BatchInsert.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Rabbit\DB\Click;
 
 use Rabbit\DB\ClickHouse\BatchInsert as ClickHouseBatchInsert;
+use Throwable;
 
 class BatchInsert extends ClickHouseBatchInsert
 {
@@ -13,9 +14,15 @@ class BatchInsert extends ClickHouseBatchInsert
     public function execute(): int
     {
         if ($this->rows) {
-            $this->db->writeStart($this->table, $this->columns);
-            $this->db->writeBlock($this->rows);
-            return (int)$this->db->writeEnd();
+            try {
+                $this->db->writeStart($this->table, $this->columns);
+                $this->db->writeBlock($this->rows);
+                return (int)$this->db->writeEnd();
+            } catch (Throwable $e) {
+                throw $e;
+            } finally {
+                $this->db->release();
+            }
         }
         return 0;
     }
